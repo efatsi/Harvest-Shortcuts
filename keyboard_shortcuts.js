9b1e194a22b225e17206287e4ef7f6692e02de06
Eli Fatsi <eli.fatsi@viget.com>
2017-07-31 09:19:54 -0600

    Updated shortcut functions to deal with new harvest layout

--- keyboard_shortcuts.js
+++ keyboard_shortcuts.js
@@ -2,17 +2,17 @@ $(document).keydown(function(e) {
   if ($(".modal-overlay").length == 0 || $(".modal-overlay.hidden").length > 0) {
     // k
     if (e.keyCode == 75) {
-      $(".js-harvest-current-view").find(".js-jump-one-day-back")[0].click()
+      $(".test-previous-day").children()[0].click()
     }
 
     // j
     else if (e.keyCode == 74) {
-      $(".js-harvest-current-view").find(".js-jump-one-day-forward")[0].click()
+      $(".test-next-day").children()[0].click()
     }
 
     // t
     else if (e.keyCode == 84) {
-      $(".js-harvest-current-view").find(".jump-to-today")[0].click()
+      $(".js-today").click()
     }
   }
 });
