b5f023998fa0ac88122970d8f27876007a91a080
Eli Fatsi <eli.fatsi@viget.com>
2018-03-21 10:56:51 -0600

    Handle UI update

--- keyboard_shortcuts.js
+++ keyboard_shortcuts.js
@@ -1,13 +1,13 @@
 $(document).keydown(function(e) {
-  if ($(".modal-overlay").length == 0 || $(".modal-overlay.hidden").length > 0) {
-    // k
-    if (e.keyCode == 75) {
-      $(".test-previous-day").children()[0].click()
+  if ($(".hui-dialog-backdrop").length == 0) {
+    // j
+    if (e.keyCode == 74) {
+      $(".test-next").children()[0].click()
     }
 
-    // j
-    else if (e.keyCode == 74) {
-      $(".test-next-day").children()[0].click()
+    // k
+    else if (e.keyCode == 75) {
+      $(".test-previous").children()[0].click()
     }
 
     // t
