c5d0c0a5f00d3ed0ad61a321586f3d85e3b72d37
Eli Fatsi <eli.fatsi@viget.com>
2019-03-27 11:03:42 -0600

    Update for new layout

--- keyboard_shortcuts.js
+++ keyboard_shortcuts.js
@@ -1,18 +1,22 @@
 $(document).keydown(function(e) {
-  if ($(".hui-dialog-backdrop").length == 0) {
+  if (
+    $(".hui-dialog-backdrop").length == 0 ||
+    $(".js-submit").length == 0 ||
+    $(".js-close").length == 0
+  ) {
     // j
     if (e.keyCode == 74) {
-      $(".test-next").children()[0].click()
+      $(".test-next")[0].click();
     }
 
     // k
     else if (e.keyCode == 75) {
-      $(".test-previous").children()[0].click()
+      $(".test-previous")[0].click();
     }
 
     // t
     else if (e.keyCode == 84) {
-      $(".js-today").click()
+      $(".js-today").click();
     }
   }
 });
